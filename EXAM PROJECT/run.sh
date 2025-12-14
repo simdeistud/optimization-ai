c22953bb90a50aad27f13a7620be97a56c65b01a
simdeistud <simone.deiana@studenti.units.it>
2025-12-14 23:33:24 +0100

    Refactored run script to compile a target from the given file and run the GE

--- EXAM PROJECT/run.sh
+++ EXAM PROJECT/run.sh
@@ -1,1 +1,9 @@
- python ponyge.py --grammar_file lilypond_grammar.bnf --initialisation uniform_tree --selection tournament --fitness_function score_ff --target /tmp/targetscore.png
+ # Files need to be copied to the correct locations inside the PonyGE2 package
+cp parameters deps/ponyge2/parameters
+cp lilypond_grammar.bnf deps/ponyge2/grammars/lilypond_grammar.bnf
+cp score_ff.py deps/ponyge2/src/fitness/score_ff.py
+source ./.venv/bin/activate
+lilypond --png -o "/tmp/targetscore" target.ly
+cd deps/ponyge2/src/
+python ponyge.py --parameters parameters
+deactivate
