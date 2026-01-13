081120d22b84537a84b31ef4916259981cc0c1a9
simdeistud <simone.deiana@studenti.units.it>
2026-01-13 17:59:57 +0100

    removed previous project's files

--- EXAM PROJECT/run.sh
+++
@@ -1,9 +0,0 @@
- # Files need to be copied to the correct locations inside the PonyGE2 package
-cp parameters deps/ponyge2/parameters
-cp lilypond_grammar.bnf deps/ponyge2/grammars/lilypond_grammar.bnf
-cp score_ff.py deps/ponyge2/src/fitness/score_ff.py
-source ./.venv/bin/activate
-lilypond --png -o "/tmp/targetscore" target.ly
-cd deps/ponyge2/src/
-python ponyge.py --parameters parameters
-deactivate
