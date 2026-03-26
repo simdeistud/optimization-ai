676c6c323d56056cc8bfe1bdd9c3f9d3c995b755
simdeistud <simone.deiana@studenti.units.it>
2026-03-27 00:13:19 +0100

    Added fitness memoization

--- EXAM PROJECT/src/real_GA.py
+++ EXAM PROJECT/src/real_GA.py
@@ -1,31 +1,48 @@
 import random
+from dataclasses import dataclass
+from typing import Optional, Tuple
 from fitness import *
 from selection import *
 
+@dataclass(slots=True)
+class Individual:
+    genome: Tuple[float, ...]
+    fitness: Optional[float] = None
+
+
+def evaluate(ind, raw_fitness):
+    if ind.fitness is None:
+        ind.fitness = float(raw_fitness(ind.genome))
+    return ind.fitness
+
+
 def init_individual(order):
-    return [random.uniform(-1, 1) for _ in range(order)]
+    return Individual(tuple(random.uniform(-1, 1) for _ in range(order)))
+
 
 def init_pop(order, n_pop):
     return [init_individual(order) for _ in range(n_pop)]
 
-def gaussian_mut(individual, p_mut=None, sigma=0.05):
-    if p_mut is None:
-        p_mut = 1 / len(individual)
 
-    mutated = []
-    for coeff in individual:
+def gaussian_mut(individual, p_mut=None, sigma=0.05):
+    genome = list(individual.genome)
+    if p_mut is None:
+        p_mut = 1 / len(genome)
+
+    for i, coeff in enumerate(genome):
         if random.random() < p_mut:
-            mutated.append(coeff + random.gauss(0, sigma))
-        else:
-            mutated.append(coeff)
-    return mutated
+            genome[i] = coeff + random.gauss(0, sigma)
+
+    return Individual(tuple(genome))
+
 
 def recomb_cross(p1, p2):
     child = []
-    for a, b in zip(p1, p2):
+    for a, b in zip(p1.genome, p2.genome):
         alpha = random.uniform(0, 1)
         child.append(alpha * a + (1 - alpha) * b)
-    return child
+    return Individual(tuple(child))
+
 
 def real_GA(
     n_pop=100,
@@ -39,23 +56,20 @@ def real_GA(
     elites_perc=5,
     tournament_k=3
 ):
-    fit = embedded_fitness(fitness, target, worN)
+    raw_fit = embedded_fitness(fitness, target, worN)
+    fit = lambda ind: evaluate(ind, raw_fit)
 
     elites_n = int(n_pop * elites_perc / 100) if elitism else 0
     pop = init_pop(order, n_pop)
     best_hist = []
 
     for _ in range(generations):
-        # sort by fitness (higher is better, since minimax_fitness = -error)
         pop.sort(key=fit, reverse=True)
 
-        # keep elites
-        elites = [ind[:] for ind in pop[:elites_n]]
-        best_hist.append(pop[0][:])
+        elites = [Individual(ind.genome, ind.fitness) for ind in pop[:elites_n]]
+        best_hist.append(Individual(pop[0].genome, pop[0].fitness))
 
-        # build next generation
         newpop = elites[:]
-
         while len(newpop) < n_pop:
             if parent_selection == tournament_pick:
                 p1 = tournament_pick(pop, fit, k=tournament_k)
